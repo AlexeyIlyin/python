f3dac5d648c285e932d39947d8c9dfcb8325630c
Alexey <avi81@bk.ru>
2021-10-12 13:27:28 +0300

    Lesson_05

---
+++ DZ/DZ_05/Task_05_01.py
@@ -0,0 +1,13 @@
+import random
+
+def rand_num (rnd_n):
+    for i in range(1, rnd_n+1,2):
+        print(f'Для {i} итерации случайное число {random.randint(1,int(i))}')
+    yield
+
+n = int (input('Введите число: '))
+
+rand_num(n)
+
+
+
